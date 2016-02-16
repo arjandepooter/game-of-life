45057931c1aa34fe09c0eb6c3af929aeaaa01720
Arjan de Pooter <mail@arjandepooter.nl>
2016-02-16 16:04:47 +0100

    Added react

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,27 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'app.js'),
+	entry: [
+		path.join(__dirname, 'src', 'app.js')
+	],
 	output: {
 		path: path.join(__dirname, 'build'),
-		filename: 'bundle.[hash].js'
+		filename: 'bundle.[hash].js',
+		publicPath: '/'
 	},
 	module: {
 		loaders: [{
 			test: /\.jsx?$/,
-			exclude: /node_modules/,
+			include: path.join(__dirname, 'src'),
 			loader: 'babel',
 			query: {
 				cacheDirectory: true,
 				presets: ['react', 'es2015']
 			}
 		}]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
 	},
 	plugins: [
 		new HtmlPlugin({
