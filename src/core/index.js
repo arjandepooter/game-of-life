1e5e733d16fb77e08a1cfc3eb7c0d7e21ad689ff
Arjan de Pooter <mail@arjandepooter.nl>
2016-02-16 12:26:18 +0100

    Use a more functional approach

--- src/core/index.js
+++ src/core/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
-import { Set, Record, Map } from 'immutable';
+import {
+	List,
+	Map,
+	Range,
+	Record,
+	Set,
+} from 'immutable';
 
 // Defines a cell in the grid
 export const Cell = Record({
@@ -8,78 +14,44 @@ export const Cell = Record({
 	y: 0
 });
 
-// Constructor
-export const State = function State() {
-	this.grid = Set();
-};
+// Set a cell to be alive
+export const set = (state, cell) => state.add(cell);
 
-// Size represents total number of alive cells
-Object.defineProperty(State.prototype, 'size', {
-	get: function() {
-		return this.grid.size;
-	}
-});
+// Set a cell to be death (remove it)
+export const unset = (state, cell) => state.remove(cell);
 
-// Check if a specified cell is alive
-State.prototype.isAlive = function(x, y) {
-	return this.grid.has(Cell({x, y}));
-};
+// Switch cell between death and alive
+export const toggle = (state, cell) => state.has(cell) ? unset(state, cell):set(state, cell);
 
-// Set status of cell to death or alive
-State.prototype.set = function(x, y, alive) {
-	const cell = Cell({x, y});
-	this.grid = alive ? this.grid.add(cell):this.grid.remove(cell);
+// Calculate next state
+export const next = state =>
+	state.filter(cell => {
+		// Living cells who need to stay alive
+		const neighbours = getNeighbours(state, cell).filter(v => v).size;
+		return neighbours >= 2 && neighbours <= 3;
+	}).concat(
+		// Death neighbours of living cells who should become alive
+		state
+			.reduce((prev, val) => getNeighbours(state, val)
+				.filterNot(v => v)
+				.keySeq()
+				.reduce(
+					(prev2, val2) => prev2.update(val2, 0, n => n + 1),
+					prev
+				), Map())
+			.filter(v => v == 3)
+			.keySeq()
+			.reduce((prev, val) => prev.add(val), Set())
+	);
 
-	return this;
-}
-
-// Shorthand to set a cell to be alive
-State.prototype.setAlive = function(x, y) {
-	return this.set(x, y, true);
-};
-
-// Shorthand to set a cell to be death
-State.prototype.setDeath = function(x, y) {
-	return this.set(x, y, false);
-};
-
-// toggle cell between death and alive
-State.prototype.toggle = function(x, y) {
-	return this.set(x, y, !this.isAlive(x, y));
-};
-
-// returns a Map off all the neighbours with cell as key and alive status as value
-State.prototype.getNeighbours = function(x, y) {
-	let neighbours = Map();
-	for(let offY = 1; offY >= -1; offY--) {
-		for(let offX = -1; offX <= 1; offX++) {
-			if(offX === 0 && offY === 0) continue;
-			const cell = Cell({x: x + offX, y: y + offY});
-			const alive = this.grid.has(cell);
-			neighbours = neighbours.set(cell, alive);
-		}
-	}
-	return neighbours;
-}
-
-// Internal function used to check if cell should be alive or not
-State.prototype._shouldTransform = function(cell, alive) {
-	const aliveNeighbours = this.getNeighbours(cell.x, cell.y).count(v => v);
-	return alive ? aliveNeighbours > 1 && aliveNeighbours < 4:aliveNeighbours === 3;
-}
-
-// Calculate new state
-State.prototype.transform = function() {
-	const newState = new State();
-	let cellsToCheck = Set();
-
-	this.grid.forEach(cell => {
-		newState.set(cell.x, cell.y, this._shouldTransform(cell, true));
-		this.getNeighbours(cell.x, cell.y).filterNot(v => v).forEach((_, cell) => {
-			cellsToCheck = cellsToCheck.add(cell);
-		});
-	});
-	cellsToCheck.forEach(cell => newState.set(cell.x, cell.y, this._shouldTransform(cell, false)));
-
-	return newState;
-}
+// get neighbours of a cell with it's alive status
+export const getNeighbours = (state, cell) =>
+	Range(-1, 2)
+		.reduce(
+			(prev, x) => Range(-1, 2)
+				.reduce(
+					(prev2, y) => prev2.add(Cell({x: cell.x + x, y: cell.y + y})),
+					prev
+				), Set())
+		.remove(cell)
+		.reduce((prev, cell) => prev.set(cell, state.has(cell)), Map());
--- test/core/index.js
+++ test/core/index.js
@@ -1,89 +1,79 @@
 'use strict';
 
-import { Cell, State } from '../../src/core';
-import Immutable from 'immutable';
+import {
+	Cell,
+	set,
+	unset,
+	toggle,
+	next,
+	getNeighbours
+} from '../../src/core';
+import * as im from 'immutable';
 import should from 'should';
 
-describe('State', function() {
-	describe('__constructor', function() {
-		it('should create a State with 0 cells alive', done => {
-			const state = new State();
-			should.exist(state.size);
-			state.size.should.be.equal(0);
-			done();
+describe('GameState functions', function() {
+	describe('set', function() {
+		it('should set a cell to alive', () => {
+			const state = im.Set();
+			const cell = Cell({x: 3, y: 5});
+			set(state, cell).has(cell).should.be.true();
 		});
 	});
 
-	describe('setAlive', function() {
-		it('should set a cell to alive', done => {
-			const state = new State();
-			state.isAlive(3, 5).should.be.false();
-			state.setAlive(3, 5).isAlive(3, 5).should.be.true();
-			done();
-		});
-	});
-
-	describe('setDeath', function() {
-		it('should set a cell to death', done => {
-			const state = new State();
-			state.setAlive(3, 5).setDeath(3, 5).isAlive(3, 5).should.be.false();
-			done();
+	describe('unset', function() {
+		it('should set a cell to death', () => {
+			const state = im.Set();
+			const cell = Cell({x: 32, y: 1});
+			unset(state, cell).has(cell).should.be.false();
 		});
 	});
 
 	describe('toggle', function() {
-		it('should toggle a cell between death and alive', done => {
-			const state = new State();
-			state.isAlive(3, 5).should.be.false();
-			state.toggle(3, 5).isAlive(3, 5).should.be.true();
-			state.toggle(3, 5).isAlive(3, 5).should.be.false();
-			state.toggle(3, 5).isAlive(3, 5).should.be.true();
-			done();
+		it('should toggle a cell between death and alive', () => {
+			let state = im.Set();
+			const cell = Cell({x: 32, y: 1});
+			state.has(cell).should.be.false();
+			state = toggle(state, cell);
+			state.has(cell).should.be.true();
+			state = toggle(state, cell);
+			state.has(cell).should.be.false();
 		});
 	});
 
-	describe('iterNeighbours', function() {
-		it('should return every neighbour with it\'s status', done => {
-			const state = new State();
-			state.setAlive(1, 1).setAlive(0, -1);
-			const neighbours = state.getNeighbours(0, 0);
+	describe('getNeighbours', function() {
+		it('should return every neighbour with it\'s status', () => {
+			const state = im.Set([Cell({x: 0, y: 0}), Cell({x: 1, y: 0})]);
+			const neighbours = getNeighbours(state, Cell({x: 1, y: 0}));
 			neighbours.size.should.be.equal(8);
-			neighbours.filter(v => v).size.should.be.equal(2);
-			done();
+			neighbours.filter(v => v).size.should.be.equal(1);
 		});
 	});
 
-	describe('transform', function() {
-		it('should transform a 2x2 square to a 2x2 square', done => {
-			const state = new State();
-			state.setAlive(0, 0).setAlive(0, 1).setAlive(1, 0).setAlive(1, 1);
-			const newState = state.transform();			
-			Immutable.is(state.grid, newState.grid).should.be.true();
-			done();
+	describe('next', function() {
+		it('should transform a 2x2 square to a 2x2 square', () => {
+			const state = im.Set([
+				Cell({x: 0, y: 0}),
+				Cell({x: 1, y: 0}),
+				Cell({x: 0, y: 1}),
+				Cell({x: 1, y: 1})
+			]);
+			const newState = next(state);
+			im.is(state, newState).should.be.true();
+			im.is(state, next(newState)).should.be.true();
 		});
 
-		it('should rotate a line with length 3', done => {
-			const state = new State();
-			state.setAlive(-1, 0).setAlive(0, 0).setAlive(1, 0);
-			const newState = state.transform();
+		it('should rotate a line with length 3', () => {
+			const state = im.Set([
+				Cell({x: -1, y: 0}),
+				Cell({x: 0, y: 0}),
+				Cell({x: 1, y: 0})
+			]);
+			const newState = next(state);
+
 			newState.size.should.be.equal(3);
-			newState.isAlive(0, -1).should.be.true();
-			newState.isAlive(0, 0).should.be.true();
-			newState.isAlive(0, 1).should.be.true();
-			done();
-		});
-
-		it('should remove the center of a 3x3 square', done => {
-			const state = new State();
-			for(let x = -1; x <= 1; x++) {
-				for(let y = -1; y <= 1; y++) {
-					state.setAlive(x, y);
-				}
-			}
-			const newState = state.transform();
-			newState.size.should.be.equal(8);
-			newState.isAlive(0, 0).should.be.false();
-			done();
+			newState.has(Cell({x: 0, y: -1})).should.be.true();
+			newState.has(Cell({x: 0, y: 0})).should.be.true();
+			newState.has(Cell({x: 0, y: 1})).should.be.true();
 		});
 	})
 });
